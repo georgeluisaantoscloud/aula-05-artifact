a0c8871837d457dee5856410561bdcb2ad4855d4
George Luis <cloudgeorge50@outlook.com>
2024-09-07 14:39:09 -0300

    Adiciona script run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+chmod +x ./linux/hello-server
+
+./linux/hello-server &
+
+sleep 5
+
+for LOGIN in Homer Bart Maggie;
+do
+    echo "$(date): $(curl -s http://localhost:13000/${LOGIN})"
+done
